8b18a9f9e48dff4d5bd47bbace579cdb435916b8
Terry Ebieto <tebieto@gmail.com>
2020-03-09 13:44:40 +0100

    Some final touch

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ window.initLogin = () => {
     $('#login-btn').html('<i class="fa fa-spinner fa-spin"></i>');     
     let form = document.login;
     let data= {email: form.email.value, password: form.password.value};
-    login(data)
+    login(data);
 }
 
 
