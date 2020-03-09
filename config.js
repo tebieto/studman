64460f07437c5df7cd72fe9b0b3e42ad5c7e6322
Terry Ebieto <tebieto@gmail.com>
2020-03-09 14:02:27 +0100

    refactored logout

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+
+const dev = "http://localhost/studman/";
+const prod= "https://tebieto.github.io/studman/";
 export const config = {
     api: "https://00cfe854-087d-44d2-ae99-fe2f2debff4e.mock.pstmn.io",
-    logout: ()=>window.location="../",
+    logout: ()=>window.location=prod,
     result: () => window.location=`${window.location.href.replace('#basis', '')}#basis`,
     getUrlVars: () => {
         var vars = {};
